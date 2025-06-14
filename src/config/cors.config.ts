38b458e84e1d6fb63223a093e1683cd208f6cced
William Dinh <hoangdinh2411@gmail.com>
2025-06-14 14:21:25 +0200

    configure cors for allow both admin and client

--- src/config/cors.config.ts
+++ src/config/cors.config.ts
@@ -2,21 +2,29 @@ import { Injectable } from '@nestjs/common';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { ConfigService } from '@nestjs/config';
 
+
+const allowOrigins = ['http://localhost:3000']
 @Injectable()
 export class CorsConfigService {
   constructor() {}
   getOptions(configServer: ConfigService): CorsOptions {
     const CORS_OPTIONS: CorsOptions = {
-      origin: '*',
+      origin: (origin, callback)=>{
+        if(!origin || [...allowOrigins, configServer.get<string>('CLIENT_HOST'),configServer.get<string>('ADMIN_HOST')].includes(origin))
+        {
+          callback(null, true)
+        }else{
+          callback(new Error(`Origin ${origin} not allowed by CORS`))
+        }
+        },
       credentials: true,
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
       allowedHeaders: [
         'Origin',
         'Content-Type',
         'Accept',
         'Set-Cookie',
         'Cookie',
-        'Authorization',
       ],
     };
     return CORS_OPTIONS;
