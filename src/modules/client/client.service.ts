145aad114987a5ee4338a59f65ea306ec62d2dea
William Dinh <hoangdinh2411@gmail.com>
2025-08-07 10:06:57 +0200

    update api to get menu

--- src/modules/client/client.service.ts
+++ src/modules/client/client.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { CouponType } from 'common/constants/enums';
 import { LIMIT_DEFAULT } from 'common/constants/variables';
+import { BlogsEntity } from 'modules/blogs/entities/blogs.entity';
 import { CategoryEntity } from 'modules/categories/entities/category.entity';
 import { CouponEntity } from 'modules/coupons/entities/coupon.entity';
 import { StoreEntity } from 'modules/stores/entities/store.entity';
+import { TopicEntity } from 'modules/topic/entities/topic.entity';
 import { DataSource } from 'typeorm';
 
 @Injectable()
@@ -28,25 +30,19 @@ export class ClientService {
       .addSelect('COUNT(coupon.id)', 'total_coupons')
       .groupBy('category.id')
       .orderBy('total_coupons', 'DESC')
-      .take(5)
+      .take(10)
       .getMany();
-    const categories = await this.dataSource
-      .getRepository(CategoryEntity)
-      .find({
-        order: {
-          name: 'ASC',
-        },
-        select: ['name', 'id', 'slug'],
-      });
+
     const categories_with_limited_store = await Promise.all(
-      categories.map(async (cat) => {
+      top_categories.map(async (cat) => {
         const stores = await this.dataSource
           .getRepository(StoreEntity)
           .createQueryBuilder('store')
-          .select(['store.id', 'store.name', 'store.slug'])
+          .select(['store.id', 'store.name', 'store.slug', 'store.rating'])
           .innerJoin('store.categories', 'category')
           .where('category.id = :id', { id: cat.id })
-          .orderBy('store.name', 'ASC')
+          .orderBy('store.rating', 'DESC')
+          .take(10)
           .getMany();
         return {
           id: cat.id,
@@ -56,11 +52,39 @@ export class ClientService {
         };
       }),
     );
+    const top_topic = await this.dataSource
+      .getRepository(TopicEntity)
+      .createQueryBuilder('topic')
+      .leftJoin('topic.blogs', 'blog')
+      .addSelect('COUNT(blog.id)', 'total_blogs')
+      .groupBy('topic.id')
+      .orderBy('total_blogs', 'DESC')
+      .take(5)
+      .getMany();
 
+    const topic_with_limited_blogs = await Promise.all(
+      top_topic.map(async (topic) => {
+        const blogs = await this.dataSource
+          .getRepository(BlogsEntity)
+          .createQueryBuilder('blog')
+          .select(['blog.id', 'blog.title', 'blog.slug', 'blog.updated_at'])
+          .innerJoin('blog.topic', 'topic')
+          .where('topic.id = :id', { id: topic.id })
+          .orderBy('blog.updated_at', 'DESC')
+          .take(5)
+          .getMany();
+        return {
+          id: topic.id,
+          name: topic.name,
+          slug: topic.slug,
+          blogs,
+        };
+      }),
+    );
     return {
-      top_categories,
+      top_categories: categories_with_limited_store,
       popular,
-      categories: categories_with_limited_store,
+      top_topic: topic_with_limited_blogs,
     };
   }
 
